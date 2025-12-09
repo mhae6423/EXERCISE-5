bb0877478b85e0c4137f32ab19aaceb74f856863
mhae6423 <doncillo.trexiemhae@llcc.edu.ph>
2025-12-09 22:06:22 +0800

    Add files via upload

---
+++ EXERCISES 5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 22;
+    float grade = 1.6;
+    char initial = 'T';
+    string name = "Trexie";
+
+    cout << age << endl;
+    cout << grade << endl;
+    cout << initial << endl;
+    cout << name << endl;
+
+    return 0;
+}
+
